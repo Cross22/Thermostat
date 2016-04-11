7d2b05cf0dd07fcc1eef10e644ac52eff2cec110
Marco <grubertm@hotmail.com>
2016-04-10 20:03:08 -0700

    Cleanup

--- Thermostat.ino
+++ Thermostat.ino
@@ -15,7 +15,6 @@ using namespace std;
 
 // One-Wire sensor interface
 #define TEMP_PIN 5
-
 #define TOUCH_CS 16
 
 // Heater relay on Pin 4 is active low and needs to be set to high ASAP
@@ -107,10 +106,6 @@ uint8_t getTemperatureF() {
   startTempReading();
   return f;
 }
-
-// Connect Redbear BLE Mini to +5 and GND
-// then connect Redbear TX to Arduino RX and vice versa
-// Everything sent via Serial class goes to either USB serial or BLE module when standalone
 
 // DS3232 breakout board pins (DIP->SO)
 // 1=1, 4=2, 6=3=32kHz 8=4=VCC 10=5=INT, 12=6=RST
@@ -613,135 +608,3 @@ uint16_t read16(File& f)
   return result;
 }
 
-
-/*
-  // This function opens a Windows Bitmap (BMP) file and
-  // displays it at the given coordinates.  It's sped up
-  // by reading many pixels worth of data at a time
-  // (rather than pixel by pixel).  Increasing the buffer
-  // size takes more of the Arduino's precious RAM but
-  // makes loading a little faster.  20 pixels seems a
-  // good balance.
-
-  #define BUFFPIXEL 20
-  void bmpDraw(const char* filename, uint8_t x, uint16_t y)
-  {
-  File bmpFile;
-  int bmpWidth, bmpHeight; // W+H in pixels
-  uint8_t bmpDepth; // Bit depth (currently must be 24)
-  uint32_t bmpImageoffset; // Start of image data in file
-  uint32_t rowSize; // Not always = bmpWidth; may have padding
-  uint8_t sdbuffer[3 * BUFFPIXEL]; // pixel buffer (R+G+B per pixel)
-  uint8_t buffidx = sizeof(sdbuffer); // Current position in sdbuffer
-  boolean goodBmp = false; // Set to true on valid header parse
-  boolean flip = true; // BMP is stored bottom-to-top
-  int w, h, row, col;
-  uint8_t r, g, b;
-  uint32_t pos = 0, startTime = millis();
-
-  if ((x >= tft.width()) || (y >= tft.height()))
-  return;
-
-  //    Serial.println();
-  //    Serial.print(F("Loading image '"));
-  //    Serial.print(filename);
-  //    Serial.println('\'');
-
-  // Open requested file on SD card
-  if ((bmpFile = SD.open(filename)) == NULL) {
-  Serial.print(F("File not found "));
-  Serial.println(filename);
-  return;
-  }
-
-  // Parse BMP header
-  if (read16(bmpFile) == 0x4D42) { // BMP signature
-  //        Serial.print(F("File size: "));
-  //        Serial.println(
-  (void)read32(bmpFile);
-  (void)read32(bmpFile); // Read & ignore creator bytes
-  bmpImageoffset = read32(bmpFile); // Start of image data
-  //        Serial.print(F("Image Offset: "));
-  //        Serial.println(bmpImageoffset, DEC);
-  // Read DIB header
-  //        Serial.print(F("Header size: "));
-  //        Serial.println(
-  (void)read32(bmpFile);
-  bmpWidth = read32(bmpFile);
-  bmpHeight = read32(bmpFile);
-  if (read16(bmpFile) == 1) { // # planes -- must be '1'
-  bmpDepth = read16(bmpFile); // bits per pixel
-  //            Serial.print(F("Bit Depth: "));
-  //            Serial.println(bmpDepth);
-  if ((bmpDepth == 24) && (read32(bmpFile) == 0)) { // 0 = uncompressed
-
-  goodBmp = true; // Supported BMP format -- proceed!
-  //                Serial.print(F("Image size: "));
-  //                Serial.print(bmpWidth);
-  //                Serial.print('x');
-  //                Serial.println(bmpHeight);
-
-  // BMP rows are padded (if needed) to 4-byte boundary
-  rowSize = (bmpWidth * 3 + 3) & ~3;
-
-  // If bmpHeight is negative, image is in top-down order.
-  // This is not canon but has been observed in the wild.
-  if (bmpHeight < 0) {
-  bmpHeight = -bmpHeight;
-  flip = false;
-  }
-
-  // Crop area to be loaded
-  w = bmpWidth;
-  h = bmpHeight;
-  if ((x + w - 1) >= tft.width())
-  w = tft.width() - x;
-  if ((y + h - 1) >= tft.height())
-  h = tft.height() - y;
-
-  // Set TFT address window to clipped image bounds
-  tft.setAddrWindow(x, y, x + w - 1, y + h - 1);
-
-  for (row = 0; row < h; row++) { // For each scanline...
-
-  // Seek to start of scan line.  It might seem labor-
-  // intensive to be doing this on every line, but this
-  // method covers a lot of gritty details like cropping
-  // and scanline padding.  Also, the seek only takes
-  // place if the file position actually needs to change
-  // (avoids a lot of cluster math in SD library).
-  if (flip) // Bitmap is stored bottom-to-top order (normal BMP)
-  pos = bmpImageoffset + (bmpHeight - 1 - row) * rowSize;
-  else // Bitmap is stored top-to-bottom
-  pos = bmpImageoffset + row * rowSize;
-  if (bmpFile.position() != pos) { // Need seek?
-  bmpFile.seek(pos);
-  buffidx = sizeof(sdbuffer); // Force buffer reload
-  }
-
-  for (col = 0; col < w; col++) { // For each pixel...
-  // Time to read more pixel data?
-  if (buffidx >= sizeof(sdbuffer)) { // Indeed
-  bmpFile.read(sdbuffer, sizeof(sdbuffer));
-  buffidx = 0; // Set index to beginning
-  }
-
-  // Convert pixel from BMP to TFT format, push to display
-  b = sdbuffer[buffidx++];
-  g = sdbuffer[buffidx++];
-  r = sdbuffer[buffidx++];
-  tft.pushColor(tft.color565(r, g, b));
-  } // end pixel
-  } // end scanline
-  Serial.print(F("Loaded in "));
-  Serial.print(millis() - startTime);
-  Serial.println(" ms");
-  } // end goodBmp
-  }
-  }
-
-  bmpFile.close();
-  if (!goodBmp)
-  Serial.println(F("BMP format not recognized."));
-  }
-*/
